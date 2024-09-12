11d4789eb5677882f923c1aac752588a367e7669
KURUMI <k.1au12um1@gmail.com>
2024-09-13 06:00:54 +0900

    layout

---
+++ pages/app.js
@@ -0,0 +1,10 @@
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+};
+
+  // `pages/_app.js`
+import '../styles/global.css';
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
